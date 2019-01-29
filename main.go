16b090fa964d6e95dd7d304efacbc4f34b3d95eb
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2019-01-29 10:35:32 -0800

    Add ticker to check every `interval` seconds

--- main.go
+++ main.go
@@ -1,45 +1,125 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"time"
 
-    "github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
+	"github.com/NVIDIA/gpu-monitoring-tools/bindings/go/nvml"
 )
 
+// multiValueFlag implements the flag.Value interface for a list of strings
+// that can be passed using `--foo bar --foo baz` to produce [bar, baz].
+type multiValueFlag []string
+
+// String returns the string representation of the multi value flag.
+func (mvf *multiValueFlag) String() string {
+	return "[" + strings.Join(*mvf, ", ") + "]"
+}
+
+// Set sets one part of the multi value flag.
+func (mvf *multiValueFlag) Set(value string) error {
+	*mvf = append(*mvf, value)
+	return nil
+}
+
+var (
+	// ignores keeps track of all the processes that we need to ignore.  The
+	// given string is compiled into a regex before it is matched.
+	ignores multiValueFlag
+
+	// refresh keeps track of the refresh interval where we check for GPU idle.
+	refresh time.Duration
+)
+
+// fatalOnErr fatals on errors.
 func fatalOnErr(err error) {
-    if err != nil {
-        fmt.Printf("Fatal error: %s\n", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
+// isIgnoredProcessName returns if the input string matches one of the ignored
+// process regexes.
+func isIgnoredProcessName(pn string) bool {
+	for _, ignore := range ignores {
+		matched, err := regexp.Match(ignore, []byte(pn))
+		if err != nil {
+			return false
+		}
+		if matched == true {
+			return true
+		}
+	}
+	return false
+}
+
+// isGPUIdle checks `count` number of devices to see if any of them are running
+// non-ignored processes.
+func isGPUIdle(count uint) (bool, error) {
+	for id := uint(0); id < count; id++ {
+		device, err := nvml.NewDevice(id)
+		if err != nil {
+			return false, err
+		}
+
+		info, err := device.GetAllRunningProcesses()
+		if err != nil {
+			return false, err
+		}
+
+		for i := range info {
+			ignored, err := isIgnoredProcessName(info[i].Name)
+			if err != nil {
+				return false, err
+			}
+
+			// Found a process running on the GPU, we are done.
+			if ignored == false {
+				return false, nil
+			}
+		}
+	}
+	// GPU is idle.
+	return true, nil
 }
 
 func main() {
-    nvml.Init()
-    defer nvml.Shutdown()
+	nvml.Init()
+	defer nvml.Shutdown()
 
-    count, err := nvml.GetDeviceCount()
-    fatalOnErr(err)
-    fmt.Printf("Found %d devices\n", count)
+	// Get the device count, this should not change once we are running.
+	count, err := nvml.GetDeviceCount()
+	fatalOnErr(err)
 
-    for id := uint(0); id < count; id++ {
-        device, err := nvml.NewDevice(id)
-        fatalOnErr(err)
+	// Once per interval ticker.
+	ticker := time.NewTicker(refresh)
+	defer ticker.Stop()
 
-        info, err := device.GetAllRunningProcesses()
-        fatalOnErr(err)
+	// TODO: Handle signal for interrupt etc.
+	for {
+		select {
+		case <-ticker.C:
+			idle, err := isGPUIdle(count)
+			fatalOnErr(err)
 
-        if len(info) == 0 {
-            fmt.Printf("Device %d is idle ...\n", id)
-        } else {
-            for i := range info {
-                ii := info[i]
-                fmt.Printf("Device %d running %v [%v] type=%v mem=%v\n",
-                            id, ii.Name, ii.PID, ii.Type, ii.MemoryUsed)
-            }
-        }
-    }
-    fmt.Printf("Done...\n")
+			if idle {
+				println("GPU is idle ...")
+			} else {
+				println("GPU is busy ...")
+			}
+		}
+	}
 }
 
-
+func init() {
+	flag.Var(&ignores, "ignore", "list of processes to ignore (always on like XORG)")
+	flag.Var(&ignores, "i", "list of processes to ignore (always on like XORG) (short)")
+	flag.DurationVar(&refresh, "refresh", 10*time.Second, "time in duration that we check the GPU for")
+	flag.DurationVar(&refresh, "r", 10*time.Second, "time in duration that we check the GPU for (short)")
+	flag.Parse()
+}
